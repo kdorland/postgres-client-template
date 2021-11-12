039725a23288375f15a60a650db2202087a2d3c7
Kristian Dorland <kristian@dorland.dk>
2021-11-12 10:47:08 +0100

    Nogle kodekommentarer der forklarer hvad der sker.

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const { Client } = require("pg");
 const express = require("express");
 
+/**
+ * Følgende parametre bruges til at forbinde til databasen.
+ * PORT er den port som webserveren her kører på.
+ * DB_USER er brugernavnet til databasen.
+ * DB_HOST er serveren som databasen kører på. Enten localhost eller en anden server.
+ * DB_NAME er det navn som databasen har.
+ * DB_PW er password til DB_USER.
+ * DB_PORT er porten til databasen. Det plejer at være 5432, så den behøver man nok ikke ændre.
+ */
 const PORT = process.env.PORT || 8080;
 const DB_USER = process.env.DB_USER || "postgres";
 const DB_HOST = process.env.DB_HOST || "localhost";
@@ -8,15 +17,32 @@ const DB_NAME = process.env.DB_NAME || "kristians-database";
 const DB_PW = process.env.DB_PW || "password";
 const DB_PORT = process.env.DB_PORT || 5432;
 
-if (!process.env.DB_NAME || !process.env.DB_PW) {
+/**
+ * I stedet for at ændre på DB-værdierne i koden herover, er det bedre at gøre det som
+ * en del af den måde man kører programmet på. Hver DB-værdi kan sættes i terminalen
+ * inden man kører programmet. Det gør man sådan her:
+ * 
+ * $ export DB_NAME="kristians-database"
+ * 
+ * Nu er DB_NAME sat til "kristians-database" når programmet kører, uden at man har
+ * ændret i JavaScript-koden. Dette skal gøres hver gang du åbner en ny terminal.
+ * Det skal helst gøres både for DB_NAME, DB_PW, DB_USER og DB_HOST.
+ * PORT og DB_PORT plejer man ikke at ændre.
+ */
+if (!process.env.DB_NAME || !process.env.DB_PW || !process.env.DB_USER) {
   console.warn("Husk at sætte databasenavn, password og user via env vars.");
-  console.warn("Sæt fx databasenavn sådan her (i bash):", `export DB_NAME="kristians-database"`);
+  console.warn("Eksempel på at sætte databasenavn i terminalen:");
+  console.warn(`export DB_NAME="kristians-database"`);
   console.warn("Lige nu er databasenavn sat til:", DB_NAME);
 } else {
   console.log("Postgres database:", DB_NAME);
   console.log("Postgres user:", DB_USER);
 }
 
+/*
+ * Herunder laves web-serveren. Den indeholder din html (fra public-folderen)
+ * og API'en så der er forbindelse videre til databasen fra JavaScript. Det er "to i en".
+ */
 const app = express();
 const client = new Client({
   user: DB_USER,
@@ -30,14 +56,25 @@ client.connect();
 app.use(express.text());
 app.use(express.static("public"))
 
+/*
+ * Her defineres API'en.
+ * Jeg har været doven og har blot lavet et "endpoint" som bruges til at sende 
+ * SQL ned i databasen. Det er en usikker løsning der åbner op for "sql injections".
+ * I praksis bør man nok sikre at brugeren i DB_USER kun har "read only" adgang på 
+ * databasen, og kun til relevante tabeller.
+ */
 app.post("/postgresql/query", async (req, res) => {
   try {
+    // Lav query
     queryData = await client.query(req.body);
+    // Giv svar tilbage til JavaScript
     res.json({
       "ok": true,
       "data": queryData.rows,
     })
   } catch (error) {
+    // Hvis query fejler, fanges det her.
+    // Send fejlbesked tilbage til JavaScript
     res.json({
       "ok": false,
       "message": error.message,
@@ -45,4 +82,5 @@ app.post("/postgresql/query", async (req, res) => {
   }
 });
 
+// Web-serveren startes.
 app.listen(PORT, () => console.log(`Serveren kører på http://localhost:${PORT}`));
